9a3a534382d9c7f72e0becc2310f1d3fb5ab3fd8
BiplobSorder <biplob.code.bd@gmail.com>
2025-08-06 09:23:55 +0600

    I have Added New Paragraf Tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <h1 className="bg-green-400">This is Biplob Sorder </h1>
+     <h1 className="bg-green-400 text-center">This is Biplob Sorder </h1>
+     <p className="text-pink-500">HEllO WORLD</p>
     </div>
   );
 }
